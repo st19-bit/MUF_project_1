d118fe8cc3c0f86aa8f42780f958726fac1aa896
st19-bit <bernhard.hirt@gmx.at>
2020-06-02 09:47:29 +0200

    Berni 1

--- app/src/main/java/com/example/mundf_project_v1/fragment/Monitoring_Fragment.java
+++ app/src/main/java/com/example/mundf_project_v1/fragment/Monitoring_Fragment.java
@@ -1,6 +1,27 @@
 package com.example.mundf_project_v1.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mundf_project_v1.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Monitoring_Fragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.monitoring_fragment,container,false);
+
+        return v;
+    }
 }
